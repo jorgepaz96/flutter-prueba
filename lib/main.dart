572e13e630043bd549a7dceb135ab2401e909946
jorgepaz96 <pazmissael@gmail.com>
2021-04-02 11:43:54 -0500

    Swiper

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,46 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
  
 void main() => runApp(MyApp());
  
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  double  _width = 100.0;
-  double  _height = 100.0;
-  Color _color = Colors.blue;
-  BorderRadius _borderRadius = BorderRadius.circular(10);
-
-  void _cambiarContainer(){
-    final random = Random();
-    _width = random.nextInt(350).toDouble();
-    _height = random.nextInt(350).toDouble();
-    _color = Color.fromRGBO(random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
-    _borderRadius = BorderRadius.circular(random.nextInt(50).toDouble());
-    setState(() {  });
-  }
-
+class MyApp extends StatelessWidget {
+  List<String> images = [
+    "https://as.com/meristation/imagenes/2021/01/20/noticias/1611162270_013847_1611162672_noticia_normal.jpg",
+    "https://sm.ign.com/t/ign_latam/screenshot/default/goku-susanoo_7by3.1280.jpg",
+    "https://www.tierragamer.com/wp-content/uploads/2018/11/Super_Saiyajin_Vegeta.jpg",
+    "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/03/Majin-Vegeta-de-Dragon-Ball-1.jpg?fit=1280%2C720&quality=80&ssl=1"
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'AnimatedContainer',
+      title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('swiper'),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 500),
-                  curve: Curves.fastOutSlowIn,
-                  width: _width,
-                  height: _height,
-                  decoration: BoxDecoration(
-                    color: _color,
-                    borderRadius: _borderRadius
-                  ),
-                ),
-              ),
-            ),
-            RaisedButton(
-              onPressed:_cambiarContainer,
-              child: Text("Cambiar container"),
-            )
-          ],
-        ),
+        body: Container(
+            child: _swiper(),
+          ),        
+      ),
+    );
+  }
+
+  Widget _swiper() {
+    return Container(
+      width: double.infinity,
+      height: 250.0,
+      child: Swiper(
+        viewportFraction: 0.8,
+        scale: 0.9,
+        itemBuilder: (BuildContext context,int index){
+          return new Image.network(
+            images[index],
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: images.length,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
       ),
     );
   }
