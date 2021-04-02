2aba83007a405fd656e42ebc36e0265c29ccd92a
jorgepaz96 <pazmissael@gmail.com>
2021-04-02 08:51:59 -0500

    Consumiendo Api rest

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/Gif.dart';
+import 'package:http/http.dart' as http; 
  
 void main() => runApp(MyApp());
  
@@ -8,15 +12,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  
+  Future<List<Gif>> _listadoGifs;
 
-  List<Persona> _personas = [
-    Persona("jose", "meza", "9094894984"),
-    Persona("jorge", "paz", "8446548491"),
-    Persona("missael", "paz", "8446548491"),
-    Persona("raul", "paz", "8446548491"),
-    Persona("tulio", "paz", "8446548491"),
-  ];
-
+  Future<List<Gif>> _getGifs() async{
+    final response = await http.get(Uri.https("api.giphy.com", '/v1/gifs/trending', {'api_key' : '10qznyNOQ4HHdQfBqvBMJBQzNQu40GWl','limit':'10','rating':'g'}));
+    List<Gif> gifs = [];
+    if (response.statusCode == 200) {
+      String body = utf8.decode(response.bodyBytes);
+      final jsonData = jsonDecode(body);      
+      for (var item in jsonData["data"]) {        
+        gifs.add(
+          Gif(item["title"],item["images"]["downsized"]["url"])
+        );
+      }
+      return gifs;
+    }else{
+      throw Exception("Falló la conexión");
+    }
+  }
+  @override
+  void initState() { 
+    super.initState();
+    _listadoGifs =  _getGifs();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,62 +44,38 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
-        body: ListView.builder(
-          itemCount: _personas.length,
-          itemBuilder: (context,index){
-            return ListTile(
-              onTap: (){
-                this._borrarPersona(context, _personas[index]);
-              },
-              title: Text(_personas[index].name + ' ' + _personas[index].lastname),
-              subtitle: Text(_personas[index].phone),
-              leading: CircleAvatar(
-                child: Text(_personas[index].name.substring(0,1)),
-              ),
-              trailing: Icon(Icons.arrow_forward_ios),
+        body: FutureBuilder(
+          future: _listadoGifs,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {              
+              return GridView.count(
+                crossAxisCount: 2,
+                children: listGifs(snapshot.data),
+              );
+            }else if (snapshot.hasError){
+              print(snapshot.error);
+              return Text("aioz");
+            }
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          }
+          },
         )
       ),
     );
   }
-  _borrarPersona(context, Persona persona){
-    showDialog(
-      context: context, 
-      builder: (_) => new AlertDialog(
-        title: Text("Eliminar contacto"),
-        content: Text("¿Estas seguro de eliminar a "+ persona.name + ' ?'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: (){
-              Navigator.pop(context);
-            }, 
-            child: Text("Cancelar")
-          ),
-          TextButton(
-            onPressed: (){
-              print(persona.name);
-              this._personas.remove(persona);
-              this.setState(() { });
-              Navigator.pop(context);
-            }, 
-            child: Text("Borrar",style: TextStyle(color: Colors.red),)
-          ),
-        ],
-      )
-    );
-  }
-}
 
-
-class Persona {
-  String name;
-  String lastname;
-  String phone;
-
-  Persona(name, lastname, phone){
-    this.name = name;
-    this.lastname = lastname;
-    this.phone = phone;
+  List<Widget> listGifs(List<Gif> data) {
+    List<Widget> gifs = [];
+    for (var gif in data) {
+      gifs.add(
+        Card(child: Column(
+          children: [
+            Expanded(child: Image.network(gif.url,fit: BoxFit.fill,))            
+          ],
+        ))
+      );
+    }
+    return gifs;
   }
 }
