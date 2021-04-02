4e0cce0f540de4f0cb9b107b749eb3c84c2bee3e
jorgepaz96 <pazmissael@gmail.com>
2021-04-01 22:40:50 -0500

    Clases para consumir api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(MiApp());
 
 class MiApp extends StatelessWidget {
@@ -18,61 +19,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  bool _subscrito = false;
-
+  Empresa _facebook = new Empresa(
+    "Facebook",
+    "Jorge missael",
+    100
+  );
+  Empresa _google = new Empresa("google", "Missael paz", 50000);
+  
+  @override
+    void initState() {      
+      super.initState();
+      print(_facebook.nombre);
+      print(_facebook.propietario);
+      print(_facebook.ingresoAnual);
+    }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Alert Dialog"),),
+        appBar: AppBar(title: Text("Clases"),),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              RaisedButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                child: Text("Suscribirme a este canala", style: TextStyle(fontSize: 20),),
-                onPressed: (){
-                  _mostrarAlerta(context);
-                },
-              ),
-              SizedBox(height: 100,),
-              Text(_subscrito? "Subscrito" : "No subscrito", style: TextStyle(fontSize: 20),)
-            ],
-          ),
-        )
-      );
-  }
-  void _mostrarAlerta(BuildContext context){
-  showDialog(
-    context: context,
-    barrierDismissible: false,
-    builder: (_) => new AlertDialog(
-          title: new Text("Hola"),
-          content: new Text(_subscrito? "Quieres desjar de seguir este canal?":"Quieres suscribirte?"),
-          actions: <Widget>[
-            TextButton(
-              child: Text("Cancelar"),
-              onPressed: (){
-                print("No");
-                
-                Navigator.pop(context);
-              },
-            ),
-            TextButton(
-              child: Text("Si quiero!"),
-              onPressed: (){
-                print("Si");
-                setState(() {
-                  _subscrito = !_subscrito;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            
-          ],
-        ));
+          child: Text(_google.ingresoAnual.toString(),style: TextStyle(fontSize: 20),)
+          )
+        );
+      
   }
 }
 
+class Empresa {
+  String nombre;
+  String propietario;
+  int ingresoAnual;
+
+  Empresa(String nombre, String propietario, int ingreso){
+    this.nombre = nombre;
+    this.propietario = propietario;
+    this.ingresoAnual = ingreso;
+  }
+
+}
