c03f9ac788ca90852ac898950a57960bc6aea89b
jorgepaz96 <pazmissael@gmail.com>
2021-04-01 23:45:00 -0500

    Api rest - list

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,86 @@
 import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
 
-void main() => runApp(MiApp());
+  List<Persona> _personas = [
+    Persona("jose", "meza", "9094894984"),
+    Persona("jorge", "paz", "8446548491"),
+    Persona("missael", "paz", "8446548491"),
+    Persona("raul", "paz", "8446548491"),
+    Persona("tulio", "paz", "8446548491"),
+  ];
 
-class MiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Mi App",
-      home: Home(),
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: ListView.builder(
+          itemCount: _personas.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              onTap: (){
+                this._borrarPersona(context, _personas[index]);
+              },
+              title: Text(_personas[index].name + ' ' + _personas[index].lastname),
+              subtitle: Text(_personas[index].phone),
+              leading: CircleAvatar(
+                child: Text(_personas[index].name.substring(0,1)),
+              ),
+              trailing: Icon(Icons.arrow_forward_ios),
+            );
+          }
+        )
+      ),
+    );
+  }
+  _borrarPersona(context, Persona persona){
+    showDialog(
+      context: context, 
+      builder: (_) => new AlertDialog(
+        title: Text("Eliminar contacto"),
+        content: Text("¿Estas seguro de eliminar a "+ persona.name + ' ?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: (){
+              Navigator.pop(context);
+            }, 
+            child: Text("Cancelar")
+          ),
+          TextButton(
+            onPressed: (){
+              print(persona.name);
+              this._personas.remove(persona);
+              this.setState(() { });
+              Navigator.pop(context);
+            }, 
+            child: Text("Borrar",style: TextStyle(color: Colors.red),)
+          ),
+        ],
+      )
     );
   }
 }
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
 
-class _HomeState extends State<Home> {
-  Empresa _facebook = new Empresa(
-    "Facebook",
-    "Jorge missael",
-    100
-  );
-  Empresa _google = new Empresa("google", "Missael paz", 50000);
-  
-  @override
-    void initState() {      
-      super.initState();
-      print(_facebook.nombre);
-      print(_facebook.propietario);
-      print(_facebook.ingresoAnual);
-    }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Clases"),),
-        body: Center(
-          child: Text(_google.ingresoAnual.toString(),style: TextStyle(fontSize: 20),)
-          )
-        );
-      
+class Persona {
+  String name;
+  String lastname;
+  String phone;
+
+  Persona(name, lastname, phone){
+    this.name = name;
+    this.lastname = lastname;
+    this.phone = phone;
   }
-}
-
-class Empresa {
-  String nombre;
-  String propietario;
-  int ingresoAnual;
-
-  Empresa(String nombre, String propietario, int ingreso){
-    this.nombre = nombre;
-    this.propietario = propietario;
-    this.ingresoAnual = ingreso;
-  }
-
 }
