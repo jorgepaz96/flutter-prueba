3da1ea08db261167386f304e241dadc28188cf50
jorgepaz96 <pazmissael@gmail.com>
2021-04-02 12:15:33 -0500

    Sharedpreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
- 
+import 'package:shared_preferences/shared_preferences.dart';
+
 void main() => runApp(MyApp());
  
-class MyApp extends StatelessWidget {
-  List<String> images = [
-    "https://as.com/meristation/imagenes/2021/01/20/noticias/1611162270_013847_1611162672_noticia_normal.jpg",
-    "https://sm.ign.com/t/ign_latam/screenshot/default/goku-susanoo_7by3.1280.jpg",
-    "https://www.tierragamer.com/wp-content/uploads/2018/11/Super_Saiyajin_Vegeta.jpg",
-    "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2021/03/Majin-Vegeta-de-Dragon-Ball-1.jpg?fit=1280%2C720&quality=80&ssl=1"
-  ];
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int value = 0;
+  _changeValue()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      value = value + 1;
+      prefs.setInt("value", value);
+    });
+  }
+  @override
+  void initState() { 
+    super.initState();
+    _cargarPreferencias();
+  }
+  _cargarPreferencias()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      value = prefs.getInt("value") ?? 0;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('swiper'),
+          title: Text('Preferences'),
         ),
-        body: Container(
-            child: _swiper(),
-          ),        
-      ),
-    );
-  }
-
-  Widget _swiper() {
-    return Container(
-      width: double.infinity,
-      height: 250.0,
-      child: Swiper(
-        viewportFraction: 0.8,
-        scale: 0.9,
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network(
-            images[index],
-            fit: BoxFit.fill,
-          );
-        },
-        itemCount: images.length,
-        pagination: new SwiperPagination(),
-        control: new SwiperControl(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text(value.toString(), style: TextStyle(fontSize: 50),),
+              ),
+              FlatButton(onPressed: _changeValue, child: Text("Aumentar valor"))
+            ],
+          ),
+        ),
       ),
     );
   }
